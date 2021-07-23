6e10469549c506e79e6194f6ae0f2af4bc823dc6
ksound22 <chrisjay967@gmail.com>
2021-07-23 17:11:58 +0200

    Finished responsiveness

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+// Scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
+// Form selections
+
+// Dark theme selections
+
+// Hamburger menu function
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// scroll to top function
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Formspree function
+
+// Dark theme toggle function
+
+// Smooth scroll
