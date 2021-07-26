b5a4f38e38d7530570bf6161dd787fbdc15ec06c
ksound22 <chrisjay967@gmail.com>
2021-07-26 16:08:03 +0100

    Fixed hamburger menu issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,21 @@ const nav = document.querySelector("nav");
 // Scroll to top selection
 const scrollUp = document.querySelector("#scroll-up");
 
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+console.log(navLink);
+
 // Hamburger menu function
 burger.addEventListener("click", () => {
   ul.classList.toggle("show");
 });
+
+// Close hamburger menu when a link is clicked
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
 
 // scroll to top functionality
 scrollUp.addEventListener("click", () => {
